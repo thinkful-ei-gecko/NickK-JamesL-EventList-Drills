9527fd2f54e914bbd09f50e14adf0baa3c9d350c
NKerner <nickkerner59@gmail.com>
2019-08-15 16:53:27 -0400

    working fizzbuzz drill

--- index2.js
+++ index2.js
@@ -1,1 +1,27 @@
 // your code here
+//create event listner on submit
+function fizzbuzz(countTo){
+    let myArr = [];
+    for(let i =1;i <= countTo;i++){
+       
+        if(i % 15 === 0){
+            return 'fizzbuzz'
+        }
+        if(i % 3 === 0){
+            return 'fizz'
+        }
+        if(i % 5 === 0){
+            return 'buzz'
+        }
+            myArr.push([i])
+        }
+            return myArr;
+    }
+
+    $().on('submit',)
+
+
+
+
+
+//
