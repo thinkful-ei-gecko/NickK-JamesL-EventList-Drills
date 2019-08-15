1220d22c389099511898f68b5b670d7d38842760
James Lee <jameswlee109@gmail.com>
2019-08-15 15:32:14 -0500

    first commit for FizzBuzz

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 
-//any thumbnail that is clicked on it becomes the target
-$('.thumbnail').on('click', e => {
+// //any thumbnail that is clicked on it becomes the target
+// $('.thumbnail').on('click', e => {
     
-    //show clicked img on the top
-    const heroCat = $('.hero img'); //get a handle to top cat img
-    const clickedCat = $(e.target);//get handle of clicked cat
+//     //show clicked img on the top
+//     const heroCat = $('.hero img'); //get a handle to top cat img
+//     const clickedCat = $(e.target);//get handle of clicked cat
     
-    //get img src frm clickedCat
-    const imgSource = clickedCat.attr('src');
+//     //get img src frm clickedCat
+//     const imgSource = clickedCat.attr('src');
 
-    //get alt attr frm clickedCat
-    const imgAlt = clickedCat.attr('alt')
+//     //get alt attr frm clickedCat
+//     const imgAlt = clickedCat.attr('alt')
     
-    //update hero with img src
-    heroCat.attr('src', imgSource);
+//     //update hero with img src
+//     heroCat.attr('src', imgSource);
     
-    //update hero with alt attr
-    heroCat.attr('alt', imgAlt);
+//     //update hero with alt attr
+//     heroCat.attr('alt', imgAlt);
     
-    console.log(e.target);
+//     console.log(e.target);
 
 
-    //also update the alt attr of top img
-    //test keyboard interactivity
-  });
+//     //also update the alt attr of top img
+//     //test keyboard interactivity
+//   });
+
+
