5d334a89f32e91b36d6dd4b50877b851b0d3688a
James Lee <jameswlee109@gmail.com>
2019-08-15 14:57:49 -0500

    cat carousel drill complete

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-function catImageClicks(){
-    $('.thumbnail').on('click', e => {
-        console.log(e.target);
-        let clickedImg = $(e.target).clone();
-        $('.hero img').replaceWith(clickedImg);
-    });
-}
+
+//any thumbnail that is clicked on it becomes the target
+$('.thumbnail').on('click', e => {
+    
+    //show clicked img on the top
+    const heroCat = $('.hero img'); //get a handle to top cat img
+    const clickedCat = $(e.target);//get handle of clicked cat
+    
+    //get img src frm clickedCat
+    const imgSource = clickedCat.attr('src');
+
+    //get alt attr frm clickedCat
+    const imgAlt = clickedCat.attr('alt')
+    
+    //update hero with img src
+    heroCat.attr('src', imgSource);
+    
+    //update hero with alt attr
+    heroCat.attr('alt', imgAlt);
+    
+    console.log(e.target);
 
 
-
-
-$(catImageClicks);
+    //also update the alt attr of top img
+    //test keyboard interactivity
+  });
